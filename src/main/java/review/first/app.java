0cd76b4d5230490f970e1ef873a5a26bbacdcffa
Nizeimana Emmanuel <nemmanuel2@hotmail.com>
2022-12-11 12:19:02 -0600

    commit

---
+++ src/main/java/review/first/app.java
@@ -0,0 +1,18 @@
+package review.first;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class app
+
+{
+    public static void main( String[] args )
+  
+    {
+    	System.setProperty("webdriver.chrome.driver","driver\\chromedriver.exe");
+    	WebDriver driver= new ChromeDriver();
+    	driver.get("https://www.google.com/");
+//    	
+        System.out.println( "Hello World!" );
+    }
+}
